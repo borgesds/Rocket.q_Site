92b1ccc7187580a4e3370d61bdc0f9f6135c1958
borgesds <borges.an@hotmail.com>
2022-05-22 20:05:44 -0300

    up RoomController and parts

--- src/route.js
+++ src/route.js
@@ -2,16 +2,20 @@ const express = require('express')
 
 // Buscar o arquivo dentro da pasta controllers
 const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
 
 // mostrar rotas .ejs
-route.get('/', (req, res) => res.render("index"))
-route.get('/room', (req, res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
+
+route.get('/room/:room', (req, res) => res.render("room"))
+
 //Rota marcar ou excluir ('/arquivo/num.sala/pergunta/check ou delete'). Formato de dentro da modal que tem que passar informação:
 route.post('/room/:room/:question/:action', QuestionController.index)
+route.post('/room/create-room', RoomController.create)
 
 // exportar o route
 module.exports = route
