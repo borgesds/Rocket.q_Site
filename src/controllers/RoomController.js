837276580a14a28862bf3f75d4faa2d6d9ba630f
borgesds <borges.an@hotmail.com>
2022-05-27 20:58:05 -0300

    up Arlet pass, verific pass, delete questions and open room

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -41,13 +41,21 @@ module.exports = {
     async open(req, res) {
         const db = await Database()
         const roomId = req.params.room;
-    
+
         // Buscar a perguntas
         const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
 
         const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
 
-        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead})
+        let isNoQuestions
+
+        if (questions.length == 0) {
+            if (questionsRead.length == 0) {
+                isNoQuestions = true
+            }
+        }
+
+        res.render("room", { roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions })
     },
 
     enter(req, res) {
